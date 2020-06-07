5481f01b7e7a2bad179ad46c1c9e5168e3088494
Simon Dieckmann <simon.dieckmann@tngtech.com>
2020-06-07 02:01:36 +0200

    rework everything

--- server.js
+++ server.js
@@ -1,256 +1,105 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const mongodb = require("mongodb");
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongodb = require('mongodb');
 const ObjectID = mongodb.ObjectID;
 const cors = require('cors');
 
 const url = require('url');
 
-const ENTITIES = 'entities';
-const SQUARES = 'squares';
-const MAPS = 'entities';
+const BOARDS = 'boards';
+const PIECES = 'pieces';
+const CELLS = 'cells';
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-const uriTest = "mongodb+srv://dbUser:dbUserPassword@cluster0-udc2e.mongodb.net/test";
-const uriDev = "mongodb+srv://dbUser:dbUserPassword@cluster0-udc2e.mongodb.net/dev";
-const uri = "mongodb+srv://dbUser:dbUserPassword@cluster0-udc2e.mongodb.net/prod";
+const uri = 'mongodb+srv://dbUser:dbUserPassword@cluster0-udc2e.mongodb.net/prod';
 
-const server = app.listen(process.env.PORT || 8080, function () {
-    const port = server.address().port;
-    console.log("App now running on port", port);
+const server = app.listen(process.env.PORT || 8080, () => {
+    console.log('App now running on port', server.address().port);
 });
 
-const setupSocketIO = ({io, entitiesChangeStream, squaresChangeStream}) => io.on('connection', socket => {
-    console.log('Connection!');
-
-    entitiesChangeStream.on('change', change => {
-        console.log('Entities changed', change);
-
-        socket.emit('update', change);
-    });
-
-    squaresChangeStream.on('change', change => {
-        console.log('Squares changed', change);
-
-        socket.emit('update', change);
-    });
-});
-
-let dbDev;
-let dbTest;
 let database;
 
-const connectWithDatabase = uri => {
-    mongodb.MongoClient.connect(uri, (err, client) => {
-        if (err) {
-            console.log(err);
-            process.exit(1);
-        }
-
-        const env = uri.split('/').pop();
-
-        if (env === 'dev') {
-            dbDev = client.db();
-        } else if (env === 'test') {
-            dbTest = client.db();
-        } else {
-            database = client.db();
-        }
-
-        console.log(`Database connection on ${env} ready`);
-
-        setupSocketIO({
-            io: require('socket.io').listen(server),
-            entitiesChangeStream: client.db(env).collection('entities').watch(),
-            squaresChangeStream: client.db(env).collection('squares').watch()
-        });
-    });
-};
-
-connectWithDatabase(uriDev);
-connectWithDatabase(uriTest);
-connectWithDatabase(uri);
+mongodb.MongoClient.connect(uri, (err, client) => {
+    if (err) {
+        console.log(err);
+        process.exit(1);
+    }
+    database = client.db();
+    console.log('Database connection ready');
+});
 
 const handleError = (res, reason, message, code) => {
-    console.log("ERROR: " + reason);
-    res.status(code || 500).json({"error": message});
+    console.log('ERROR: ' + reason);
+    res.status(code || 500).json({'error': message});
 };
 
-const getDatabase = () => database;
-const getTestDatabase = req => url.parse(req.url, true).query.experimental ? dbDev : dbTest;
+const getEntities = collectionName => (req, res) => {
+    const queryObject = url.parse(req.url, true).query;
+    const query = {
+        ...(queryObject.boardId && {boardId: queryObject.boardId}),
+        ...(queryObject.changedSince && {changeDate: { $gte: new Date(queryObject.changedSince)}})
+    };
 
-app.get("/api/maps", (req, res) => {
-    getDatabase(req).collection(MAPS).find({}).toArray((err, docs) => {
-        if (err) {
-            handleError(res, err.message, "Failed to fetch maps.");
-        } else {
-            res.status(200).json(docs);
-        }
-    });
-});
+    database.collection(collectionName).find(query).toArray()
+        .then(docs => res.status(200).json(docs))
+        .catch(err => handleError(res, err.message, `Failed to fetch ${collectionName}.`))
+};
 
-app.post("/api/maps", (req, res) => {
-    const newMap = req.body;
-    newMap.createDate = new Date();
-
-    if (!req.body.name) {
-        handleError(res, "Invalid request", "Must provide a name.", 400);
-    } else {
-        getDatabase(req).collection(MAPS).insertOne(newMap, (err, doc) => {
-            if (err) {
-                handleError(res, err.message, "Failed to create new map.");
-            } else {
-                res.status(201).json(doc.ops[0]);
-            }
-        });
-    }
-});
-
-app.get("/api/maps/:id", (req, res) => {
-    getDatabase(req).collection(MAPS).findOne({_id: new ObjectID(req.params.id)}, (err, doc) => {
-        if (err) {
-            handleError(res, err.message, "Failed to get map");
-        } else {
-            res.status(200).json(doc);
-        }
-    });
-});
-
-app.put("/api/maps/:id", (req, res) => {
-    const updateDoc = req.body;
-    delete updateDoc._id;
-
-    getDatabase(req).collection(MAPS).updateOne({_id: new ObjectID(req.params.id)}, updateDoc, function (err) {
-        if (err) {
-            handleError(res, err.message, "Failed to update map");
-        } else {
-            updateDoc._id = req.params.id;
-            res.status(200).json(updateDoc);
-        }
-    });
-});
-
-app.delete("/api/maps/:id", (req, res) => {
-    getDatabase(req).collection(MAPS).deleteOne({_id: new ObjectID(req.params.id)}, function (err) {
-        if (err) {
-            handleError(res, err.message, "Failed to delete map");
-        } else {
-            res.status(200).json(req.params.id);
-        }
-    });
-});
-
-app.get("/api/entities", (req, res) => {
-    getTestDatabase(req).collection(ENTITIES).find({}).toArray((err, docs) => {
-        if (err) {
-            handleError(res, err.message, "Failed to fetch entities.");
-        } else {
-            res.status(200).json(docs);
-        }
-    });
-});
-
-app.post("/api/entities", (req, res) => {
+const postEntity = collectionName => (req, res) => {
     const newEntity = req.body;
-    newEntity.createDate = new Date();
 
-    if (!req.body.name) {
-        handleError(res, "Invalid request", "Must provide a name.", 400);
+    if (!newEntity) {
+        handleError(res, 'Invalid request', 'Must provide data.', 400);
     } else {
-        getTestDatabase(req).collection(ENTITIES).insertOne(newEntity, (err, doc) => {
-            if (err) {
-                handleError(res, err.message, "Failed to create new entity.");
-            } else {
-                res.status(201).json(doc.ops[0]);
-            }
-        });
+        newEntity.createDate = new Date();
+        database.collection(collectionName).insertOne(newEntity)
+            .then(doc => res.status(201).json(doc.ops[0]))
+            .catch(err => handleError(res, err.message, 'Failed to create new entity.'));
     }
-});
+};
 
-app.get("/api/entities/:id", (req, res) => {
-    getTestDatabase(req).collection(ENTITIES).findOne({_id: new ObjectID(req.params.id)}, (err, doc) => {
-        if (err) {
-            handleError(res, err.message, "Failed to get entity");
-        } else {
-            res.status(200).json(doc);
-        }
-    });
-});
+const getEntity = collectionName => (req, res) => {
+    database.collection(collectionName).findOne({_id: new ObjectID(req.params.id)})
+        .then(doc => res.status(200).json(doc))
+        .catch(err => handleError(res, err.message, 'Failed to get entity.'));
+};
 
-app.put("/api/entities/:id", (req, res) => {
+const putEntity = collectionName => (req, res) => {
     const updateDoc = req.body;
-    delete updateDoc._id;
 
-    getTestDatabase(req).collection(ENTITIES).updateOne({_id: new ObjectID(req.params.id)}, updateDoc, function (err) {
-        if (err) {
-            handleError(res, err.message, "Failed to update entity");
-        } else {
-            updateDoc._id = req.params.id;
-            res.status(200).json(updateDoc);
-        }
-    });
-});
+    if (!updateDoc) {
+        handleError(res, 'Invalid request', 'Must provide data.', 400);
 
-app.delete("/api/entities/:id", (req, res) => {
-    getTestDatabase(req).collection(ENTITIES).deleteOne({_id: new ObjectID(req.params.id)}, function (err) {
-        if (err) {
-            handleError(res, err.message, "Failed to delete entity");
-        } else {
-            res.status(200).json(req.params.id);
-        }
-    });
-});
-
-app.get("/api/squares", function (req, res) {
-    getTestDatabase(req).collection(SQUARES).find({}).toArray((err, docs) => {
-        if (err) {
-            handleError(res, err.message, "Failed to fetch squares.");
-        } else {
-            res.status(200).json(docs);
-        }
-    });
-});
-
-app.post("/api/squares", (req, res) => {
-    const newSquare = req.body;
-    newSquare.createDate = new Date();
-
-    if (!req.body) {
-        handleError(res, "Invalid request", "Must provide data.", 400);
     } else {
-        getTestDatabase(req).collection(SQUARES).insertOne(newSquare, (err, doc) => {
-            if (err) {
-                handleError(res, err.message, "Failed to create new square.");
-            } else {
-                res.status(201).json(doc.ops[0]);
-            }
-        });
+        delete updateDoc._id;
+        updateDoc['$set'].changeDate = new Date();
+
+        database.collection(collectionName).updateOne({_id: new ObjectID(req.params.id)}, updateDoc)
+            .then(() => {
+                updateDoc._id = req.params.id;
+                res.status(200).json(updateDoc);
+            })
+            .catch(err => handleError(res, err.message, 'Failed to update entity.'));
     }
+};
+
+const deleteEntity = collectionName => (req, res) => {
+    database.collection(collectionName).deleteOne({_id: new ObjectID(req.params.id)})
+        .then(() => res.status(200).json(req.params.id))
+        .catch(err => handleError(res, err.message, 'Failed to delete entity.'));
+};
+
+[BOARDS, PIECES, CELLS].forEach(entityType => {
+    app.get(`/api/${entityType}`, getEntities(entityType));
+    app.post(`/api/${entityType}`, postEntity(entityType));
+    app.get(`/api/${entityType}/:id`, getEntity(entityType));
+    app.put(`/api/${entityType}/:id`, putEntity(entityType));
+    app.delete(`/api/${entityType}/:id`, deleteEntity(entityType));
 });
 
-app.put("/api/squares/:id", (req, res) => {
-    const updateDoc = req.body;
-    delete updateDoc._id;
-
-    getTestDatabase(req).collection(SQUARES).updateOne({_id: new ObjectID(req.params.id)}, updateDoc, err => {
-        if (err) {
-            handleError(res, err.message, "Failed to update entity");
-        } else {
-            updateDoc._id = req.params.id;
-            res.status(200).json(updateDoc);
-        }
-    });
-});
-
-app.delete("/api/squares/:id", (req, res) => {
-    getTestDatabase(req).collection(SQUARES).deleteOne({_id: new ObjectID(req.params.id)}, function (err) {
-        if (err) {
-            handleError(res, err.message, "Failed to delete entity");
-        } else {
-            res.status(200).json(req.params.id);
-        }
-    });
+app.get('/api/currentDate', (req, res) => {
+    res.status(200).json({currentDate: new Date()})
 });
